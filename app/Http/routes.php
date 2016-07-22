87e58aaf137516fc380f947ed76deed586dac44c
Abhimanyu310 <abhip310@gmail.com>
2016-07-22 20:33:48 +0530

    Modified dashboard view, and add post implemented to DB

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,3 +36,8 @@ Route::get('/dashboard', [
     'as' => 'dashboard',
     'middleware' => 'auth'
 ]);
+
+Route::post('/createpost', [
+    'uses' => 'PostController@postCreatePost',
+    'as' => 'post.create'
+]);
